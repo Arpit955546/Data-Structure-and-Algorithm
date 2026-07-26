bc11ba3d65d32cf08d11872977f7fd92338018c7
Arpit955546 <9555463148arpit@gmail.com>
2026-07-26 14:52:27 +0530

    Time: 3 ms (6.69%), Space: 15.1 MB (7.73%) - LeetHub

---
+++ 0213-house-robber-ii/0213-house-robber-ii.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+int solve(vector<int> nums,int i,int n,vector<int> &dp){
+    if(i>=n){
+        return 0;
+    }
+    if(dp[i]!=-1){
+        return dp[i];
+    }
+    int take =nums[i]+solve(nums,i+2,n,dp);
+    int skip=solve(nums,i+1,n,dp);
+    return dp[i]=max(take,skip);
+}
+    int rob(vector<int>& nums) {
+        if(nums.size()==1){
+            return nums[0];
+        }
+        vector<int> dp1(nums.size()+1,-1);
+        vector<int> dp2(nums.size()+1,-1);
+      int x= solve(nums,0,nums.size()-1,dp1);
+      int y=solve(nums,1,nums.size(),dp2);
+      return max(x,y);  
+    }
+};
